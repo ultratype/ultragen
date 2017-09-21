14034ea9a044cc5d829c5b9364b5853d1cd42191
ultratype <ultratypebot@gmail.com>
2017-09-21 16:13:27 +0000

    Add generator class

---
+++ src/Generator.js
@@ -0,0 +1,9 @@
+const request = require("./request");
+class Generator {
+    constructor(initialUsername, initialPass) {
+        this.uname = initialUsername;
+        this.pass = initialPass;
+    }
+}
+
+module.exports = Generator;
