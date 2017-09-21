4a3c78a1fb547a5a07090edb16781948d92835cf
ultratype <ultratypebot@gmail.com>
2017-09-21 16:14:23 +0000

    Improve index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-const register = require('./register'); // Async function that registers accounts
-let init = async () => {
-    let res = await register('__username123__', '123asd123');
-    console.log(res);
-}
-init().catch(e => {
+const Generator = require('./Generator'),
+    init = async () => {
+        // TODO
+    }
+init().catch(e => { // Run init in promise-based context
     console.error('Init exception:');
     console.error(e);
 });
