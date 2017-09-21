5071d3e99de5aeb60cdfe63717fceb7a737db8a6
ultratype <ultratypebot@gmail.com>
2017-09-21 16:10:12 +0000

    Add node version control in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+node_version=$(node -v) # Get the current node version
+expected_node_version="8.5.0"
+if [ "$node_version" -ne "$expected_node_version" ]
+then
+    # Use node 8.5.0
+    echo "start.sh: using node 8.5.0."
+    nvm use 8.5.0
+fi
+# Launch node normally
 clear
 node --harmony src/index.js
