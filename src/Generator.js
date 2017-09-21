8895f0c3f5949fb85d90d12fe3d997f1d45680af
ultratype <ultratypebot@gmail.com>
2017-09-21 16:18:13 +0000

    Add async request generator

--- src/Generator.js
+++ src/Generator.js
@@ -1,8 +1,16 @@
-const request = require("./request");
+const registerAccount = require("./request");
 class Generator {
     constructor(initialUsername, initialPass) {
         this.uname = initialUsername;
         this.pass = initialPass;
+        this.genIndex = 0;
+    }
+    generate(amount) {
+        for await (; this.genIndex < amount; ++this.genIndex) {
+            const username = `${uname}_${genIndex}`,
+                    password = this.pass,
+                    res = await registerAccount(username, password);
+        }
     }
 }
 
