3e6998f04bf24ef8451413d940b6bd988ff927e3
ultratype <ultratypebot@gmail.com>
2017-09-22 13:24:06 +0000

    Add config object generator

--- src/Generator.js
+++ src/Generator.js
@@ -9,12 +9,23 @@ class Generator {
 }
 Generator.prototype.generate = async (amount, ctx) => {
     console.log('Generating ' + amount + ' accounts');
+    var configOut = {
+        "multi_account": true,
+        "accounts": [ ]
+    };
     for (; ctx.genIndex < amount; ++ctx.genIndex) {
         const username = `${ctx.uname}_${ctx.genIndex}`,
             password = ctx.pass;
         let res = await registerAccount(username, password);
-        console.log("Generated account:", res);
+        console.log('Generated accout #' + ctx.genIndex);
+        configOut.accounts.push({
+            "user": username,
+            "pass": password,
+            "wpm": 120,
+            "accuracy": 0.99
+        });
     }
+    console.log(`Finished generating ${ctx.genIndex} accounts.`);
 }
 
 module.exports = Generator;
