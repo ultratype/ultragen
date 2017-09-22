af1515664f948fc67a21ef250c43095fa26d337d
ultratype <ultratypebot@gmail.com>
2017-09-22 19:07:54 +0000

    Improve start script

--- start.sh
+++ start.sh
@@ -2,11 +2,12 @@
 
 node_version=$(node -v) # Get the current node version
 expected_node_version="8.5.0"
-if [ "$node_version" -ne "$expected_node_version" ] ;
+if [ "$node_version" != "$expected_node_version" ]
 then
     # Use node 8.5.0
-    echo "start.sh: using node 8.5.0."
-    nvm use 8.5.0
+    echo "start.sh: using node $expected_node_version..."
+    ~/.nvm/nvm.sh use 8.5.0
+    echo "Set node version to $expected_node_version via nvm."
 fi
 # Launch node normally
 # clear
